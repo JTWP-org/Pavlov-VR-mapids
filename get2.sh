f730f3d5954c787993fe1851f5670db1db67b5b2
oneSALTycracker <STEAM@JTWP>
2023-08-06 02:46:40 -0400

    SEND IT

--- get2.sh
+++ get2.sh
@@ -26,19 +26,19 @@ curl -X  GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=0"  -H 'A
 sleep 5s
 
 
-cat "${dir}/bin/api0.dump" | jq -j ' .data[]| .stats.popularity_rank_position, "$UGC", .id,"$", .name,"$", .profile_url,"$", .submitted_by.username," ", .submitted_by.profile_url," ", .submitted_by.avatar.original, " DOWNLOADS TODAY; ", .stats.downloads_today," TOTAL; ", .stats.downloads_total," RANK; ", "\n"  ' | tr -d ","  > "${dir}/bin/PAVLOV-mapsRAW.txt"
+cat "${dir}/bin/api0.dump" | jq -j ' .data[]| .stats.popularity_rank_position, "$UGC", .id,"$", .name,"$", .profile_url,"$", .submitted_by.username," ", .submitted_by.profile_url," ", .submitted_by.avatar.original, " DOWNLOADS TODAY; ", .stats.downloads_today," TOTAL; ", .stats.downloads_total," RANK; ", "\n"  ' > "${dir}/bin/PAVLOV-mapsRAW.txt"
 
-cat "${dir}/bin/api1.dump" | jq -j ' .data[]| .stats.popularity_rank_position, "$UGC", .id,"$", .name,"$", .profile_url,"$", .submitted_by.username," ", .submitted_by.profile_url," ", .submitted_by.avatar.original, " DOWNLOADS TODAY; ", .stats.downloads_today," TOTAL; ", .stats.downloads_total," RANK; ", "\n"  ' | tr -d "," >> "${dir}/bin/PAVLOV-mapsRAW.txt"
+cat "${dir}/bin/api1.dump" | jq -j ' .data[]| .stats.popularity_rank_position, "$UGC", .id,"$", .name,"$", .profile_url,"$", .submitted_by.username," ", .submitted_by.profile_url," ", .submitted_by.avatar.original, " DOWNLOADS TODAY; ", .stats.downloads_today," TOTAL; ", .stats.downloads_total," RANK; ", "\n"  ' >> "${dir}/bin/PAVLOV-mapsRAW.txt"
 
-cat "${dir}/bin/api2.dump" | jq -j ' .data[]| .stats.popularity_rank_position, "$UGC", .id,"$", .name,"$", .profile_url,"$", .submitted_by.username," ", .submitted_by.profile_url," ", .submitted_by.avatar.original, " DOWNLOADS TODAY; ", .stats.downloads_today," TOTAL; ", .stats.downloads_total," RANK; ", "\n"  ' | tr -d ","  >> "${dir}/bin/PAVLOV-mapsRAW.txt"
+cat "${dir}/bin/api2.dump" | jq -j ' .data[]| .stats.popularity_rank_position, "$UGC", .id,"$", .name,"$", .profile_url,"$", .submitted_by.username," ", .submitted_by.profile_url," ", .submitted_by.avatar.original, " DOWNLOADS TODAY; ", .stats.downloads_today," TOTAL; ", .stats.downloads_total," RANK; ", "\n"  ' >> "${dir}/bin/PAVLOV-mapsRAW.txt"
 
-cat "${dir}/bin/api3.dump" | jq -j ' .data[]| .stats.popularity_rank_position, "$UGC", .id,"$", .name,"$", .profile_url,"$", .submitted_by.username," ", .submitted_by.profile_url," ", .submitted_by.avatar.original, " DOWNLOADS TODAY; ", .stats.downloads_today," TOTAL; ", .stats.downloads_total," RANK; ", "\n"  ' | tr -d ","  >> "${dir}/bin/PAVLOV-mapsRAW.txt"t
+cat "${dir}/bin/api3.dump" | jq -j ' .data[]| .stats.popularity_rank_position, "$UGC", .id,"$", .name,"$", .profile_url,"$", .submitted_by.username," ", .submitted_by.profile_url," ", .submitted_by.avatar.original, " DOWNLOADS TODAY; ", .stats.downloads_today," TOTAL; ", .stats.downloads_total," RANK; ", "\n"  ' >> "${dir}/bin/PAVLOV-mapsRAW.txt"t
 
-cat "${dir}/bin/api4.dump" | jq -j ' .data[]| .stats.popularity_rank_position, "$UGC", .id,"$", .name,"$", .profile_url,"$", .submitted_by.username," ", .submitted_by.profile_url," ", .submitted_by.avatar.original, " DOWNLOADS TODAY; ", .stats.downloads_today," TOTAL; ", .stats.downloads_total," RANK; ", "\n"  ' | tr -d ","  >> "${dir}/bin/PAVLOV-mapsRAW.txt"
+cat "${dir}/bin/api4.dump" | jq -j ' .data[]| .stats.popularity_rank_position, "$UGC", .id,"$", .name,"$", .profile_url,"$", .submitted_by.username," ", .submitted_by.profile_url," ", .submitted_by.avatar.original, " DOWNLOADS TODAY; ", .stats.downloads_today," TOTAL; ", .stats.downloads_total," RANK; ", "\n"  ' >> "${dir}/bin/PAVLOV-mapsRAW.txt"
 
 
 echo "UGC,NAME,URL" > "${dir}/rankedPavlov.csv"
-cat "${dir}/PAVLOV-mapsRAW.txt" | tr -d "|"  | sort -n  | awk -F"$" '{print $2","$3","$4}' >> "${dir}/rankedPavlov.csv"
+cat "${dir}/PAVLOV-maps.txt" | tr -d "|"  | sort -n  | awk -F"$" '{print $2","$3","$4}' >> "${dir}/rankedPavlov.csv"
 
 cat "${dir}/rankedPavlov.csv" | tr -d "|"  |  column -s "\," -t > table-rankedPavlov.txt
 
@@ -46,9 +46,9 @@ cat "${dir}/rankedPavlov.csv" | tr -d "|"  |  column -s "\," -t > table-rankedPa
 
 #bash "${dir}/discord.sh" \
 #  --webhook-url="${HOOK}" \
-#  --file "${dir}/rankedPavlov.csv" \
-#  --text "todays pavlov maps by most downloads in the day \\n list rebuilt every hour "
+##  --file "${dir}/rankedPavlov.csv" \
+ # --text "todays pavlov maps by most downloads in the day \\n list rebuilt every hour "
 
 
 
-git commit -am "SEND IT" ; git push --force
+git commit -am "SEND IT" ; git push
