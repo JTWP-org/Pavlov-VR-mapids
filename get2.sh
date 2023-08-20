87812492cef21e72f3cc8150c054ae0cac9fe9b2
oneSALTycracker <STEAM@JTWP>
2023-08-20 11:25:15 -0400

    Send it Sun Aug 20 11:25:15 AM EDT 2023

--- get2.sh
+++ get2.sh
@@ -18,15 +18,15 @@ do
 
   cd $(echo $dir)
 
-  curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=0"   -H 'Accept: application/json' -H 'linux-Modio-Platform' > "${dir}/bin/api0.dump"
+  curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=0"   -H 'Accept: application/json' -H 'X-Modio-Platform: linux' > "${dir}/bin/api0.dump"
   sleep 5s
-  curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=0"  -H 'Accept: application/json' -H 'linux-Modio-Platform' > "${dir}/bin/api1.dump"
+  curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=0"  -H 'Accept: application/json' -H 'X-Modio-Platform: linux' > "${dir}/bin/api1.dump"
   sleep 5s
-  curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=0"  -H 'Accept: application/json' -H 'linux-Modio-Platform' > "${dir}/bin/api2.dump"
+  curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=0"  -H 'Accept: application/json' -H 'X-Modio-Platform: linux' > "${dir}/bin/api2.dump"
   sleep 5s
-  curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=0"  -H 'Accept: application/json' -H 'linux-Modio-Platform' > "${dir}/bin/api3.dump"
+  curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=0"  -H 'Accept: application/json' -H 'X-Modio-Platform: linux' > "${dir}/bin/api3.dump"
   sleep 5s
-  curl -X  GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=0"  -H 'Accept: application/json' -H 'linux-Modio-Platform' > "${dir}/bin/api4.dump"
+  curl -X  GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=0"  -H 'Accept: application/json' -H 'X-Modio-Platform: linux' > "${dir}/bin/api4.dump"
   sleep 5s
 
   echo "UGC,NAME,URL" > "${dir}/rankedPavlov.csv"
