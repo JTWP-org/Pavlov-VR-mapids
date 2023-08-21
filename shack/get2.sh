4c286e6f7b366359d677077237e82a6b1cdc0d59
oneSALTycracker <STEAM@JTWP>
2023-08-21 18:45:07 -0400

    Send it Mon Aug 21 06:45:07 PM EDT 2023

--- shack/get2.sh
+++ shack/get2.sh
@@ -2,48 +2,22 @@
 # SET THIS TO WHERE SCRIPT IS 
 dir="/home/steam/code2/modio"
 
-# WHERE IS THE FINISHED LIST GOING
-while true
-do
+  source ".vault"
 
-  filename=$( echo $(date +%m-%d-%y- )rankedPavlov.tsv )
+ 
 
-  #SET GLOBAL 
-  export filename
-  export dir
-
-  #SET PASSWORDS 
-  source "${dir}/.vault"
-
-
-  cd $(echo $dir)
-
-  curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=0"     -H 'Accept: application/json' -H 'X-Modio-Platform: windows'  |  jq -c '.data[], "\\n"' | grep windows | jq . > "${dir}/bin/api0.dump"
+  curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=0"     -H 'Accept: application/json' -H 'X-Modio-Platform: Oculus'  > data.json  
   sleep 5s
-  curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=100"   -H 'Accept: application/json' -H 'X-Modio-Platform: windows'  |  jq -c '.data[], "\\n"'  | grep windows | jq . >> "${dir}/bin/api0.dump"
+  curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=100"     -H 'Accept: application/json' -H 'X-Modio-Platform: Oculus'  >> data.json  
   sleep 5s
-  curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=200"   -H 'Accept: application/json' -H 'X-Modio-Platform: windows'  |  jq -c '.data[], "\\n"'  | grep windows | jq . >> "${dir}/bin/api0.dump"
+  curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=200"     -H 'Accept: application/json' -H 'X-Modio-Platform: Oculus'  >> data.json  
   sleep 5s
-  curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=300"   -H 'Accept: application/json' -H 'X-Modio-Platform: windows'  |  jq -c '.data[], "\\n"'  | grep windows | jq . >> "${dir}/bin/api0.dump"
-  sleep 5s
-  curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=400"   -H 'Accept: application/json' -H 'X-Modio-Platform: windows'  |  jq -c '.data[], "\\n"'  | grep windows | jq . >> "${dir}/bin/api0.dump"
-  sleep 5s
-  curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=500"   -H 'Accept: application/json' -H 'X-Modio-Platform: windows'  |  jq -c '.data[], "\\n"'  | grep windows | jq . >> "${dir}/bin/api0.dump"
-  sleep 5s
-  curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=0"   -H 'Accept: application/json' -H 'X-Modio-Platform: linux'  |  jq -c '.data[], "\\n"' | grep linux | jq . >> "${dir}/bin/api0.dump"
-  sleep 5s
-  curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=100"   -H 'Accept: application/json' -H 'X-Modio-Platform: linux'  |  jq -c '.data[], "\\n"'  | grep linux | jq . >> "${dir}/bin/api0.dump"
-  sleep 5s
-  curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=200"   -H 'Accept: application/json' -H 'X-Modio-Platform: linux'  |  jq -c '.data[], "\\n"'  | grep linux | jq . >> "${dir}/bin/api0.dump"
-  sleep 5s
-  curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=300"   -H 'Accept: application/json' -H 'X-Modio-Platform: linux'  |  jq -c '.data[], "\\n"'  | grep linux | jq . >> "${dir}/bin/api0.dump"
-  sleep 5s
-  curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=400"   -H 'Accept: application/json' -H 'X-Modio-Platform: linux'  |  jq -c '.data[], "\\n"'  | grep linux | jq . >> "${dir}/bin/api0.dump"
+  curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=300"     -H 'Accept: application/json' -H 'X-Modio-Platform: Oculus'  >> data.json  
+  sleep 5s> data.json  
+  curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=400"     -H 'Accept: application/json' -H 'X-Modio-Platform: Oculus'  >> data.json  
   sleep 5s
 
-
-  echo "" > "sorted3.json"
-  cat /home/steam/code2/modio/bin/api0.dump | sort -u | uniq | jq -c  >> "sorted3.json"
+cat data.json | jq -c J 
 
 
   echo "UGC,NAME,URL" > "${dir}/rankedPavlov.csv"
