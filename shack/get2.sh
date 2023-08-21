fa41a8aa262b59a9d38bd0262ab272205e5c2083
oneSALTycracker <STEAM@JTWP>
2023-08-21 19:56:21 -0400

    quick fix to dirs for running outside of file

--- shack/get2.sh
+++ shack/get2.sh
@@ -3,7 +3,7 @@
 
 dir="/home/steam/code2/modio/shack"
 
-  source ".vault"
+  source "${dir}.vault"
 
  
 touch "${dir}/bin/data.json"
@@ -13,7 +13,7 @@ touch "${dir}/bin/data.json"
   curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=100"     -H 'Accept: application/json' -H 'X-Modio-Platform: Oculus'  >> "${dir}/bin/data.json"  
   sleep 5s
 
-  cat "${dir}/bin/data.json"   | jq -j ' .data[]| .stats.popularity_rank_position, "$UGC", .id,"$", .name,"$", .profile_url,"$", "$", .submitted_by.username,"$ ", .submitted_by.profile_url," ", .submitted_by.avatar.original, " DOWNLOADS TODAY; ", .stats.downloads_today," TOTAL; ", .stats.downloads_total," RANK; ", " \n"  ' | tr -d "|" | tr -d ","  | sort -n  | awk -F"$" '{print $2","$3","$4"}' > shackMaps.json
+  cat "${dir}/bin/data.json"   | jq -j ' .data[]| .stats.popularity_rank_position, "$UGC", .id,"$", .name,"$", .profile_url,"$", "$", .submitted_by.username,"$ ", .submitted_by.profile_url," ", .submitted_by.avatar.original, " DOWNLOADS TODAY; ", .stats.downloads_today," TOTAL; ", .stats.downloads_total," RANK; ", " \n"  ' | tr -d "|" | tr -d ","  | sort -n  | awk -F"$" '{print $2","$3","$4"}' > ${dir}/shackMaps.json
 
 
 
