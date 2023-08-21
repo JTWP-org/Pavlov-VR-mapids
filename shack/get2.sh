b032e69d4b7d1b6a5b60a1f567b4c01b727cc7f0
oneSALTycracker <STEAM@JTWP>
2023-08-21 18:54:15 -0400

    Send it Mon Aug 21 06:54:15 PM EDT 2023

--- shack/get2.sh
+++ shack/get2.sh
@@ -12,7 +12,7 @@ touch "${dir}/bin/data.json"
   curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=100"     -H 'Accept: application/json' -H 'X-Modio-Platform: Oculus'  >> "${dir}/bin/data.json"  
   sleep 5s
 
-  cat bin/data.json | jq -J keys
+  cat bin/data.json | jq -j ' .data[]| .stats.popularity_rank_position, "$UGC", .id,"$", .name,"$", .profile_url,"$", .submitted_by.username," ", .submitted_by.profile_url," ", .submitted_by.avatar.original, " DOWNLOADS TODAY; ", .stats.downloads_today," TOTAL; ", .stats.downloads_total," RANK; ", "\n"  ' | tr -d "|" | tr -d ","  | sort -n  | awk -F"$" '{print $2","$3","$4}' > shackMaps.json
 
 
 
