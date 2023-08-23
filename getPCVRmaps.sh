740ae3b3905a6dba119c530d7b2c8ae089b34ce9
oneSALTycracker <STEAM@JTWP>
2023-08-23 19:24:18 -0400

    Send it Wed Aug 23 07:24:18 PM EDT 2023

--- getPCVRmaps.sh
+++ getPCVRmaps.sh
@@ -47,6 +47,6 @@ curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=800"     -
 sleep 5s
 
 
-cat "/home/steam/code2/modio/bin/data.json"   | jq -j ' .data[]| "$",.stats.popularity_rank_position, "$UGC", .id,"$", .name,"$", .profile_url,"$", "$", .submitted_by.username,"$ ", .submitted_by.profile_url," ", .submitted_by.avatar.original, " DOWNLOADS TODAY; ", .stats.downloads_today," TOTAL; ", .stats.downloads_total," RANK; ", " \n"  ' | tr -d "|" | tr -d ","  | sort -n  | awk -F"$" '{print $2","$3","$4","$5}' > /home/steam/code2/modio/pcvrMaps.json
+cat "/home/steam/code2/modio/bin/data.json"   | jq -j ' .data[]| "$",.stats.popularity_rank_position, "$UGC", .id,"$", .name,"$", .profile_url,"$", "$", .submitted_by.username,"$ ", .submitted_by.profile_url," ", .submitted_by.avatar.original, " DOWNLOADS TODAY; ", .stats.downloads_today," TOTAL; ", .stats.downloads_total," RANK; ", " \n"  ' | tr -d "|" | tr -d ","  | sort -n  | awk -F"$" '{print $2","$3","$4","$5}' | uniq  > /home/steam/code2/modio/buuiltLIST-PCVR.csv
 
 
