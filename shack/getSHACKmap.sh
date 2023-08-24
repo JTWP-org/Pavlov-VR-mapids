801d99fee860b1f9d3470b2de1ade3e60a2a7b41
oneSALTycracker <STEAM@JTWP>
2023-08-24 11:30:33 -0400

    Send it Thu Aug 24 11:30:33 AM EDT 2023

--- shack/getSHACKmap.sh
+++ shack/getSHACKmap.sh
@@ -22,7 +22,7 @@ curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=300"     -
 sleep 5s
 curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=400"     -H 'Accept: application/json' -H 'X-Modio-Platform: Oculus'  >> "${dir}/shack/bin/data.json"  
 sleep 5s
-cat "${dir}/shack/bin/data.json"   | jq -j ' .data[]| "$",.stats.popularity_rank_position, "$UGC", .id,"$", .name,"$", .profile_url,"$", "$", .submitted_by.username,"$ ", .submitted_by.profile_url," ", .submitted_by.avatar.original, " DOWNLOADS TODAY; ", .stats.downloads_today," TOTAL; ", .stats.downloads_total," RANK; ", " \n"  ' | tr -d "|" | tr -d ","  | sort -n  | awk -F"$" '{print $2","$3","$4","$5}' | uniq  > "${dir}/buuiltLIST-SHACK.csv"
+cat "${dir}/shack/bin/data.json"   | jq -j ' .data[]| "$",.stats.popularity_rank_position, "$UGC", .id,"$", .name,"$", .profile_url,"$", "$", .submitted_by.username,"$ ", .submitted_by.profile_url," ", .submitted_by.avatar.original, " DOWNLOADS TODAY; ", .stats.downloads_today," TOTAL; ", .stats.downloads_total," RANK; ", " \n"  ' | tr -d "|" | tr -d ","  | sort -n  | awk -F"$" '{print $2","$3","$4","$5}' | uniq | sort -n  > "${dir}/buuiltLIST-SHACK.csv"
 
 
 git add .
