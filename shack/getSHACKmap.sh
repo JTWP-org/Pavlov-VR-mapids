f8b2aaf74d1055ea393186af941448eb36af94aa
oneSALTycracker <STEAM@JTWP>
2023-08-24 20:59:23 -0400

    Send it Thu Aug 24 08:59:23 PM EDT 2023

--- shack/getSHACKmap.sh
+++ shack/getSHACKmap.sh
@@ -13,6 +13,16 @@ else
     echo "dir check passed"
 fi
 
+curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=0"     -H 'Accept: application/json' -H 'X-Modio-Platform: Oculus' > "${dir}/shack/bin/data.json"  
+sleep 5s
+curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=100"     -H 'Accept: application/json' -H 'X-Modio-Platform: Oculus'  >> "${dir}/shack/bin/data.json"  
+sleep 5s
+curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=200"     -H 'Accept: application/json' -H 'X-Modio-Platform: Oculus'  >> "${dir}/shack/bin/data.json"  
+sleep 5s
+curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=300"     -H 'Accept: application/json' -H 'X-Modio-Platform: Oculus'  >> "${dir}/shack/bin/data.json"  
+sleep 5s
+curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=400"     -H 'Accept: application/json' -H 'X-Modio-Platform: Oculus'  >> "${dir}/shack/bin/data.json"  
+sleep 5s
 
 
 cat "${dir}/shack/bin/data.json"   | jq -j ' .data[]| "$ ",.stats.popularity_rank_position, "$ UGC", .id,"$ ", .name,"$", .profile_url, "$ ", .submitted_by.username, "$ ", .submitted_by.profile_url," $", .submitted_by.avatar.original, "$  DOWNLOADS TODAY/TOTAL; ", .stats.downloads_today,"/", .stats.downloads_total, " \n"  ' | tr -d "|" | tr -d ","  | sort -n  | awk -F"$" '{print $3","$4","$5","$6 " "$9}' | uniq | sort -n  > "${dir}/buuiltLIST-SHACK.csv"
