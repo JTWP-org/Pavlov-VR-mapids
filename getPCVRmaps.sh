5762d90276112196e8ff8170aa6f7f1cde3cfef3
oneSALTycracker <STEAM@JTWP>
2023-08-24 21:00:08 -0400

    Send it Thu Aug 24 09:00:08 PM EDT 2023

--- getPCVRmaps.sh
+++ getPCVRmaps.sh
@@ -9,9 +9,45 @@ then
     EXIT
 fi
  
-touch "${dir}/bin/data.json"
 
 
+curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=0"     -H 'Accept: application/json' -H 'X-Modio-Platform: Windows' > "${dir}/bin/data.json"  
+sleep 5s
+curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=100"     -H 'Accept: application/json' -H 'X-Modio-Platform: Windows'  >> "${dir}/bin/data.json"  
+sleep 5s
+curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=200"     -H 'Accept: application/json' -H 'X-Modio-Platform: Windows' >> "${dir}/bin/data.json"  
+sleep 5s
+curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=300"     -H 'Accept: application/json' -H 'X-Modio-Platform: Windows' >> "${dir}/bin/data.json"  
+sleep 5s
+curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=400"     -H 'Accept: application/json' -H 'X-Modio-Platform: Windows' >> "${dir}/bin/data.json"  
+sleep 5s
+curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=500"     -H 'Accept: application/json' -H 'X-Modio-Platform: Windows' >> "${dir}/bin/data.json"  
+sleep 5s
+curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=600"     -H 'Accept: application/json' -H 'X-Modio-Platform: Windows' >> "${dir}/bin/data.json"  
+sleep 5s
+curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=700"     -H 'Accept: application/json' -H 'X-Modio-Platform: Windows' >> "${dir}/bin/data.json"  
+sleep 5s
+curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=800"     -H 'Accept: application/json' -H 'X-Modio-Platform: Windows' >> "${dir}/bin/data.json"  
+sleep 5s
+
+curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=0"     -H 'Accept: application/json' -H 'X-Modio-Platform: linux' >> "${dir}/bin/data.json"  
+sleep 5s
+curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=100"     -H 'Accept: application/json' -H 'X-Modio-Platform: linux'  >> "${dir}/bin/data.json"  
+sleep 5s
+curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=200"     -H 'Accept: application/json' -H 'X-Modio-Platform: linux' >> "${dir}/bin/data.json"  
+sleep 5s
+curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=300"     -H 'Accept: application/json' -H 'X-Modio-Platform: linux'  >> "${dir}/bin/data.json"  
+sleep 5s
+curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=400"     -H 'Accept: application/json' -H 'X-Modio-Platform: linux' >> "${dir}/bin/data.json"  
+sleep 5s
+curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=500"     -H 'Accept: application/json' -H 'X-Modio-Platform: linux'  >> "${dir}/bin/data.json"  
+sleep 5s
+curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=600"     -H 'Accept: application/json' -H 'X-Modio-Platform: linux' >> "${dir}/bin/data.json"  
+sleep 5s
+curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=700"     -H 'Accept: application/json' -H 'X-Modio-Platform: linux'  >> "${dir}/bin/data.json"  
+sleep 5s
+curl -X GET "${APIKEY0}/v1/games/3959/mods?api_key=${APIKEY1}&_offset=800"     -H 'Accept: application/json' -H 'X-Modio-Platform: linux'  >> "${dir}/bin/data.json"  
+sleep 5s
 
 cat "${dir}/bin/data.json"   | jq -j ' .data[]| "$ ",.stats.popularity_rank_position, "$ UGC", .id,"$ ", .name,"$", .profile_url, "$ ", .submitted_by.username, "$ ", .submitted_by.profile_url," $", .submitted_by.avatar.original, "$  DOWNLOADS TODAY/TOTAL; ", .stats.downloads_today,"/", .stats.downloads_total, " \n"  ' | tr -d "|" | tr -d ","  | sort -n  | awk -F"$" '{print $3","$4","$5","$6 " "$9}' | uniq | sort -n > ${dir}/buuiltLIST-PCVR.csv
 
