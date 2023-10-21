7d471e19473e5df3aba330086e0909a2a9e0712a
oneSALTycracker <STEAM@JTWP>
2023-10-21 02:12:13 -0400

    AUTO PUSH just updating map list

--- run.sh
+++ run.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-cd "/home/steam/code2/modio"
 
-if [ -z "$dir" ]
-then
-    echo dir varbale is empty enter the scripts location into the varbale dir in .vault file or script wont work 
-    EXIT
-fi
- 
-source "${dir}/vault"
- 
-bash "${dir}/getPCVRmaps.sh"
-bash "${dir}/shack/getSHACKmap.sh"
+bash "/home/steam/code2/modio/getPCVRmaps.sh"
+bash "/home/steam/code2/modio/shack/getSHACKmap.sh"
